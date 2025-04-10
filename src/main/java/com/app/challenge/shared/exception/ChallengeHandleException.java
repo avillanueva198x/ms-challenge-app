f14ee0a0a704d43ee2c787becbc7a86d7df5207e
Adolfo Villanueva <avillanuevapa@proveedor.intercorp.com.pe>
2025-04-10 16:29:14 -0500

    FIX de PMD y Checkstyle

---
+++ src/main/java/com/app/challenge/shared/exception/ChallengeHandleException.java
@@ -0,0 +1,9 @@
+package com.app.challenge.shared.exception;
+
+public class ChallengeHandleException extends RuntimeException {
+
+    public ChallengeHandleException(String message) {
+        super(message);
+    }
+
+}
